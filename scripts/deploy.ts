0cd609c30c6c19650b6d5a2704ece031965fde4c
paOol <paoolkim@gmail.com>
2022-08-31 10:50:57 -0700

    update deploy type to uups

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,7 @@ const func = async (hre: {
   const { getNamedAccounts, ethers, upgrades } = hre;
   const { deployer } = await getNamedAccounts();
 
-  const DIMO_TOKEN = `0x80Ee7ec4493A1d7975ab900F94dB25ba7C688201`; //mumbai
+  const DIMO_TOKEN = `0xE261D618a959aFfFd53168Cd07D12E37B26761db`; //mainnet
   const DIMO_FOUNDATION = `0xEB0Cf16298582e4DC408a0A52d5232f1204675C3`; // temporary
 
   const StakeFactory: any = await ethers.getContractFactory("Stake", deployer);
@@ -16,13 +16,20 @@ const func = async (hre: {
     [DIMO_TOKEN],
     {
       initializer: "initialize",
+      kind: "uups",
     }
   );
 
   await stakeProxy.deployed();
 
+  const defaultAdminRoleBytes = `0x0000000000000000000000000000000000000000000000000000000000000000`;
   // replace with Multisig address later
-  await (await stakeProxy.transferOwnership(DIMO_FOUNDATION)).wait();
+  await (
+    await stakeProxy.grantRole(defaultAdminRoleBytes, DIMO_FOUNDATION)
+  ).wait();
+
+  // revoke deployer address in the future
+  // `revokeRole()`
 
   // v2
   // const StakeV2: any = await ethers.getContractFactory('StakeV2', deployer);
