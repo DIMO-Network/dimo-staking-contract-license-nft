97455c2cc80a3078c5b89f37484e3cae8741dc36
paOol <paoolkim@gmail.com>
2022-09-01 12:58:03 -0700

    update foundation address

--- deploy/00_deploy_stake.ts
+++ deploy/00_deploy_stake.ts
@@ -1,27 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 
-import { DeployFunction } from 'hardhat-deploy/types';
-import { THardhatRuntimeEnvironmentExtended } from 'helpers/types/THardhatRuntimeEnvironmentExtended';
+import { DeployFunction } from "hardhat-deploy/types";
+import { THardhatRuntimeEnvironmentExtended } from "helpers/types/THardhatRuntimeEnvironmentExtended";
 
-const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => {
+const func: DeployFunction = async (
+  hre: THardhatRuntimeEnvironmentExtended
+) => {
   const { getNamedAccounts, ethers, upgrades } = hre;
   const { deployer } = await getNamedAccounts();
 
   const DIMO_TOKEN = `0xE261D618a959aFfFd53168Cd07D12E37B26761db`; //mainnet
-  const DIMO_FOUNDATION = `0xEB0Cf16298582e4DC408a0A52d5232f1204675C3`; // temporary
+  const DIMO_FOUNDATION = `0xCcFa7c808F5b77822e4935a7230F46681c99F4Bc`; // matic
 
-  const StakeFactory: any = await ethers.getContractFactory('Stake', deployer);
+  const StakeFactory: any = await ethers.getContractFactory("Stake", deployer);
 
-  const stakeProxy: any = await upgrades.deployProxy(StakeFactory, [DIMO_TOKEN], {
-    initializer: 'initialize',
-  });
+  const stakeProxy: any = await upgrades.deployProxy(
+    StakeFactory,
+    [DIMO_TOKEN],
+    {
+      initializer: "initialize",
+    }
+  );
 
   await stakeProxy.deployed();
 
   const defaultAdminRoleBytes = `0x0000000000000000000000000000000000000000000000000000000000000000`;
   // replace with Multisig address later
-  await (await stakeProxy.grantRole(defaultAdminRoleBytes, DIMO_FOUNDATION)).wait();
+  await (
+    await stakeProxy.grantRole(defaultAdminRoleBytes, DIMO_FOUNDATION)
+  ).wait();
 
   // revoke deployer address in the future
   // `revokeRole()`
@@ -35,4 +43,4 @@ const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => 
   // console.log('test', test);
 };
 export default func;
-func.tags = ['Stake'];
+func.tags = ["Stake"];
